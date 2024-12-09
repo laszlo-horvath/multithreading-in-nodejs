d3226f01eff37f11f3def9665cc6182ec0f0d847
laszlo-horvath <horvathlaszlo88@gmail.com>
2024-12-09 20:01:57 +0100

    feat: add exec example

---
+++ child-processes/exec.js
@@ -0,0 +1,12 @@
+const { exec, execFile, spawn } = require('child_process');
+
+// exec - Executes a command in a shell, buffers the output
+exec('ls -la', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+
+  console.log('exec output:');
+  console.log(stdout);
+});
