ea9a5c5d3a7a7dd584138c64ba06cf0d8202c09d
laszlo-horvath <horvathlaszlo88@gmail.com>
2024-12-09 20:17:11 +0100

    docs: add code comments for explanation

--- forks/cpu-heavy-task.js
+++ forks/cpu-heavy-task.js
@@ -1,8 +1,12 @@
 const { cpuIntensiveCalculation } = require("./../utils/index");
 
+// Listen for message from parent process
 process.on("message", (message) => {
   if (message === "start") {
+    // Perform CPU-intensive calculation
     const sum = cpuIntensiveCalculation();
+
+    // Send result back to parent process
     process.send(sum);
   }
 });
